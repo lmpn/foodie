2c348c63f4e055fc15c26a404cd096afbc85ea3e
Luis Neto <luis.neto.dev@gmail.com>
2023-11-17 00:36:43 +0000

    wip

--- foodie-app/src/components/grid.rs
+++ foodie-app/src/components/grid.rs
@@ -2,14 +2,26 @@ use crate::{
     api::recipe_api::{get_recipes, Recipe},
     error_template::ErrorTemplate,
 };
+use itertools::Itertools;
 use leptos::*;
+
+struct Pagination {
+    pub page_size: u64,
+    pub page_number: u64,
+}
+
+impl Default for Pagination {
+    fn default() -> Self {
+        Pagination { page_size: 16, page_number: 0 }
+    }
+}
 
 #[component]
 fn RecipeCard(recipe: Recipe) -> impl IntoView {
     view! {
-        <div class="grid-item">
-            <p>{recipe.name.clone()}</p>
-            <img src={recipe.image}/>
+        <div class="w3-quarter">
+            <img src={recipe.image} style="width:100%"/>
+            <h3>{recipe.name.clone()}</h3>
         </div>
     }
 }
@@ -17,6 +29,8 @@ fn RecipeCard(recipe: Recipe) -> impl IntoView {
 #[component]
 pub fn RecipeGrid() -> impl IntoView {
     //TODO pass via argument
+    //let (count, set_count) = create_signal(5);
+    //let (page, set_page) = create_signal(0);
     let count = 5;
     let page = 0;
     let recipes = create_resource(move || (), move |_| get_recipes(count, page));
@@ -26,7 +40,21 @@ pub fn RecipeGrid() -> impl IntoView {
             .map(move |recipes| match recipes {
                 Ok(recipes) => recipes
                     .into_iter()
-                    .map(|recipe| view! {<RecipeCard recipe/>})
+                    .chunks(4)
+                    .into_iter()
+                    .map(|row| {
+                        let recipe_row = row
+                            .into_iter()
+                            .map(|recipe| {
+                                view! {<RecipeCard recipe/>}
+                            })
+                            .collect_view();
+                        view! {
+                            <div class="w3-row-padding w3-padding-16 w3-center" id="food">
+                                {recipe_row}
+                            </div>
+                        }
+                    })
                     .collect_view(),
                 Err(error) => {
                     view! { <pre class="error">"Server Error: " {error.to_string()}</pre>}
@@ -39,7 +67,14 @@ pub fn RecipeGrid() -> impl IntoView {
         <Transition fallback=move || view! {<p>"Loading..."</p>}>
             <ErrorBoundary fallback=|errors|view!{<ErrorTemplate errors=errors/>}>
                 <div class="grid-container">
-                    { render_recipes() }
+                    { move || render_recipes() }
+                </div>
+
+                <div class="w3-center w3-padding-32">
+                    <div class="w3-bar">
+                        <a href="#" class="w3-bar-item w3-button w3-hover-black">"<"</a>
+                        <a href="#" class="w3-bar-item w3-button w3-hover-black">">"</a>
+                    </div>
                 </div>
             </ErrorBoundary>
         </Transition>
--- foodie-app/src/landing.rs
+++ foodie-app/src/landing.rs
@@ -3,23 +3,26 @@ use crate::{
     client_app_state::ClientAppState,
     components::{grid::RecipeGrid, header::Header},
 };
-use leptos::*;
-use leptos_meta::*;
-use leptos_router::*;
+
+use leptos::tracing;
+use leptos::IntoView;
+use leptos::{
+    component, create_resource, create_rw_signal, create_server_action, provide_context, view,
+    Show, Transition,
+};
+use leptos_meta::{provide_meta_context, Link, Stylesheet};
+use leptos_reactive::signal_prelude::SignalUpdate;
+use leptos_reactive::SignalGet;
+use leptos_router::{Route, Router, Routes};
 
 #[component]
 pub fn Landing() -> impl IntoView {
     let state = create_rw_signal(ClientAppState::default());
     provide_context(state);
-
     let login = create_server_action::<authorization_api::Login>();
     let logout = create_server_action::<authorization_api::Logout>();
     let signup = create_server_action::<authorization_api::Signup>();
-
-    let user: Resource<
-        (usize, usize, usize),
-        Result<Option<authorization_api::AuthenticatedUser>, ServerFnError>,
-    > = create_resource(
+    let user_resource = create_resource(
         move || {
             (
                 login.version().get(),
@@ -35,38 +38,37 @@ pub fn Landing() -> impl IntoView {
             user
         },
     );
-
-    let is_user_logged_in = move || {
-        matches!(user.get(), Some(Ok(Some(_))))
-    };
+    let is_user_logged_in = move || matches!(user_resource.get(), Some(Ok(Some(_))));
 
     provide_meta_context();
 
     view! {
-        <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
-        <Stylesheet id="leptos" href="/pkg/foodie-app.css"/>
-        <Router>
-            <main>
+            <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
+                <Stylesheet id="leptos" href="/pkg/foodie-app.css"/>
+                <Router>
+                <main>
                 <Header/>
                 <Routes>
-                    <Route path="/"
-                        view=move || {
-                            view! {
-                                <Transition fallback=move || view! {<span>"Loading..."</span>} >
-                                    <Show when=move || {is_user_logged_in()}>
-                                        <RecipeGrid/>
-                                    </Show>
-                                </Transition>
-                            }}
-                    />
-                    <Route path="signup" view=move || view! { < crate::forms::signup_form::SignupForm action=signup/> }/>
-                    <Route path="login" view=move || view! { < crate::forms::login_form::LoginForm action=login /> }/>
-                    <Route path="settings" view=move || view! {
-                        <h1>"Settings"</h1>
+                <Route path="/"
+                view=move || {
+                    view! {
+                        <Transition fallback=move || view! {<span>"Loading..."</span>} >
+                            <Show when=move || {is_user_logged_in()}>
+                            <div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+                                <RecipeGrid/>
+                            </div>
+                            </Show>
+                            </Transition>
+                    }}
+                />
+                <Route path="signup" view=move || view! { < crate::forms::signup_form::SignupForm action=signup/> }/>
+                <Route path="login" view=move || view! { < crate::forms::login_form::LoginForm action=login /> }/>
+                <Route path="settings" view=move || view! {
+                    <h1>"Settings"</h1>
                         <crate::forms::logout_form::LogoutForm action=logout />
-                    }/>
-                </Routes>
-            </main>
-        </Router>
-    }
+                }/>
+            </Routes>
+                </main>
+                </Router>
+        }
 }
